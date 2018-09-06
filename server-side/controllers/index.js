37b28d2502a08f53a9f7ad0b3375f26870876a85
chunkingz <fortuneking33@yahoo.com>
2018-09-06 13:38:16 +0100

    updated api end points

--- server-side/controllers/index.js
+++ server-side/controllers/index.js
@@ -2,10 +2,9 @@
 import newOrderController from './newOrderController';
 import allOrdersController from './allOrdersController';
 import singleOrderController from './singleOrderController';
-// import updateStatus from './updateStatusController';
+import updateOrderStatusController from './updateOrderStatusController';
 import homepageController from './homepageController';
 import pageNotFound from './pageNotFound';
 
 // Export controllers
-// export { newOrder, allOrders, specificOrder, updateStatus, homePage, notFoundPage };
-export { singleOrderController, newOrderController, allOrdersController, homepageController, pageNotFound };
+export { singleOrderController, newOrderController, allOrdersController, homepageController, updateOrderStatusController, pageNotFound };
--- server-side/routes/routes.js
+++ server-side/routes/routes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import db from '../db/orders-db.js';
 
 // import  { newOrder, allOrders, specificOrder, updateStatus, homePage, notFoundPage } from '../controllers/index';
-import { pageNotFound, homepageController, allOrdersController, newOrderController, singleOrderController } from '../controllers/index';
+import { pageNotFound, homepageController, allOrdersController, newOrderController, singleOrderController, updateOrderStatusController } from '../controllers/index';
 
 
 const router = express.Router();
@@ -22,50 +22,7 @@ router.get('/api/v1/orders/:id', singleOrderController.singleOrderController);
 router.post('/api/v1/orders', newOrderController.newOrderController);
 
 // Update Order
-router.put('/api/v1/orders/:id', (req, res) => {
-    const id = parseInt(req.params.id, 10);
-    let orderFound;
-    let orderIndex;
-    db.map((order, index) => {
-        if (order.id === id) {
-            orderFound = order;
-            orderIndex = index;
-        }
-    });
-
-    if (!orderFound) {
-        return res.status(404).send({
-            status: 'false',
-            message: 'order not found',
-        });
-    }
-
-    if (!req.body.title) {
-        return res.status(400).send({
-            status: 'false',
-            message: 'title is required',
-        });
-    } else if (!req.body.description) {
-        return res.status(400).send({
-            status: 'false',
-            message: 'description is required',
-        });
-    }
-
-    const updatedorder = {
-        id: orderFound.id,
-        title: req.body.title || orderFound.title,
-        description: req.body.description || orderFound.description,
-    };
-
-    db.splice(orderIndex, 1, neworder);
-
-    return res.status(201).send({
-        status: 'true',
-        message: 'Order added statusfully',
-        updatedorder,
-    });
-});
+router.put('/api/v1/orders/:id', updateOrderStatusController.updateOrderStatusController);
 
 // delete order
 router.delete('/api/v1/orders/:id', (req, res) => {
